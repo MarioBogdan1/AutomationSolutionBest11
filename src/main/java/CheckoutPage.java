6b6fdf298b413452263bb990030376a0c8ba295c
mariobogdan <mariobogdaniancu@gmail.com>
2025-04-30 14:49:21 +0300

    Mario 5 Test

--- src/main/java/CheckoutPage.java
+++ src/main/java/CheckoutPage.java
@@ -68,8 +68,186 @@ public void clickContinueCheckoutButton(){
 public void clickCompleteOrderButton(){
     completeOrderButton.click();
 }
+@FindBy(css = ".text-muted")
+private WebElement orderCompleteMessage;
 
+        public WebElement getOrderCompleteMessage(){
 
+            return orderCompleteMessage;
+        }
+    @FindBy(linkText = "Awesome Granite Chips")
+    private WebElement awesomeChips;
 
+    public void clickAwesomeChips(){
+        awesomeChips.click();
+    }
+    @FindBy(css = ".svg-inline--fa.fa-cart-plus.fa-w-18.fa-3x ")
+    private WebElement cartIconButton;
 
+    public void clickCartIconButton(){
+        cartIconButton.click();
+    }
+    @FindBy(css = ".svg-inline--fa.fa-shopping-cart.fa-w-18 ")
+    private WebElement shoppingCartIconButton;
+
+    public void clickShoppingCartIconButton(){
+        shoppingCartIconButton.click();
+    }
+@FindBy(xpath = "(//td[@class='amount'])[1]")
+    private WebElement itemPride;
+
+    public double productPrice(){
+        String amountValue = itemPride.getText(); //$15.99
+        String cleanAmount = amountValue.replace("$", "");
+        return Double.parseDouble(cleanAmount);
+    }
+@FindBy(css = ".svg-inline--fa.fa-plus-circle.fa-w-16")
+    private WebElement plusQuantityButton;
+
+    public void clickPlusQuantityButton(){
+
+        plusQuantityButton.click();
+    }
+@FindBy(linkText = "Awesome Metal Chair")
+    private WebElement awesomeMetalChairProduct;
+
+    public void clickAwesomeMetalChairProduct(){
+        awesomeMetalChairProduct.click();
+    }
+@FindBy(css = ".svg-inline--fa.fa-heart.fa-w-16.fa-3x")
+ private WebElement wishListHartButton;
+
+    public void clickWishListHartButton(){
+        wishListHartButton.click();
+    }
+@FindBy(css = ".svg-inline--fa.fa-heart.fa-w-16")
+    private WebElement wishListHartIcon;
+
+    public void clickWishListHartIcon(){
+        wishListHartIcon.click();
+    }
+@FindBy(css = ".svg-inline--fa.fa-heart-broken.fa-w-16.fa-2x ")
+    private WebElement wishListBrokenHartIcon;
+
+    public void clickWishListBrokenHartIcon(){
+        wishListBrokenHartIcon.click();
+    }
+@FindBy(css = ".text-muted")
+    private WebElement wishListComplete;
+
+    public WebElement getWishListComplete(){
+        return wishListComplete;
+    }
+@FindBy(linkText = "Awesome Soft Shirt")
+    private WebElement awesomeShirtProduct;
+
+    public void clickAwesomeShirtProduct(){
+        awesomeShirtProduct.click();
+    }
+@FindBy(css = ".svg-inline--fa.fa-cart-plus.fa-w-18.fa-3x")
+    private WebElement shoppingCartButton;
+
+    public void clickShoppingCartButton(){
+        shoppingCartButton.click();
+    }
+@FindBy(css = ".svg-inline--fa.fa-shopping-cart.fa-w-18")
+    private WebElement shoppingIconButton;
+
+    public void clickShoppingIconButton(){
+        shoppingIconButton.click();
+    }
+@FindBy(css = ".svg-inline--fa.fa-trash.fa-w-14")
+    private WebElement trashButton;
+
+    public void clickTrashButton(){
+        trashButton.click();
+    }
+@FindBy(linkText = "Gorgeous Soft Pizza")
+    private WebElement softPizzaProduct;
+
+    public void clickSoftPizzaProduct(){
+        softPizzaProduct.click();
+    }
+@FindBy(css = ".svg-inline--fa.fa-heart.fa-w-16.fa-3x")
+private WebElement wishListButton;
+
+    public void clicWishListButton(){
+        wishListButton.click();
+    }
+@FindBy(css = ".svg-inline--fa.fa-heart.fa-w-16")
+    private WebElement wishListIcon;
+
+    public void clickWishListIcon(){
+        wishListIcon.click();
+    }
+@FindBy(css = ".svg-inline--fa.fa-cart-plus.fa-w-18.fa-2x")
+private WebElement cartButton;
+
+    public void clickCartButton(){
+        cartButton.click();
+    }
+@FindBy(css = ".svg-inline--fa.fa-shopping-cart.fa-w-18")
+private WebElement shoppingCartIconWish;
+
+    public void clickShoppingCartIconWish(){
+        shoppingCartIconWish.click();
+    }
+@FindBy(css = ".btn.btn-success")
+private WebElement getCheckoutButton;
+
+public void clickGetCheckoutButton(){
+    getCheckoutButton.click();
 }
+@FindBy(id = "first-name")
+private WebElement getFirstNameSlot;
+
+public void setGetFirstNameSlot(){
+
+    getFirstNameSlot.sendKeys("Mario");
+}
+    @FindBy(id = "last-name")
+    private WebElement getLastNameSlot;
+
+    public void setGetLastNameSlot(){
+        getLastNameSlot.sendKeys("Iancu");
+    }
+    @FindBy(id = "address")
+    private WebElement getAddressSlot;
+
+    public void setGetAddressSlot(){
+        getAddressSlot.sendKeys("Craiova");
+    }
+    @FindBy(css = ".btn.btn-success")
+    private WebElement getContinueCheckoutButton;
+
+    public void clickGetContinueCheckoutButton(){
+        getContinueCheckoutButton.click();
+    }
+    @FindBy(css = ".btn.btn-success")
+    private WebElement getCompleteOrderButton;
+
+    public void clickGetCompleteOrderButton(){
+        getCompleteOrderButton.click();
+    }
+    @FindBy(css = ".text-muted")
+    private WebElement getOrderCompleteMessage;
+
+    public WebElement getOrderCompleteMessageFinal(){
+
+        return getOrderCompleteMessage;
+    }
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
--- src/test/java/CheckoutTest.java
+++ src/test/java/CheckoutTest.java
@@ -32,7 +32,68 @@ checkoutPage.setLastNameSlot();
 checkoutPage.setAddressSlot();
 checkoutPage.clickContinueCheckoutButton();
 checkoutPage.clickCompleteOrderButton();
+assertEquals(checkoutPage.getOrderCompleteMessage().getText(),"Order Complete");
 Thread.sleep(5000);
 }
+
+@Test(description = "Changing quantity test.Asserting that the price is updated correctly.")
+    public void quantityTest(){
+        checkoutPage.clickAwesomeChips();
+        checkoutPage.clickCartIconButton();
+        checkoutPage.clickShoppingCartIconButton();
+        ExtentTestNGITestListener.getTest().log(Status.INFO, " The price of the product is: " + checkoutPage.productPrice());
+double expectedTotal = checkoutPage.productPrice() * 2;
+ExtentTestNGITestListener.getTest().log(Status.INFO, " The price of the product after updating should be: " + expectedTotal);
+checkoutPage.clickPlusQuantityButton();
+assertEquals(checkoutPage.productPrice(), expectedTotal);
+ExtentTestNGITestListener.getTest().log(Status.PASS, "The price of the product matches the expected total" + checkoutPage.productPrice() + "=" + expectedTotal);
+    }
+
+    @Test(description = "Adding and removing a product from wishlist.")
+    public void wishListTest()throws InterruptedException  {
+checkoutPage.clickAwesomeMetalChairProduct();
+checkoutPage.clickWishListHartButton();
+checkoutPage.clickWishListHartIcon();
+checkoutPage.clickWishListBrokenHartIcon();
+ExtentTestNGITestListener.getTest().log(Status.PASS, "Adding and removing a product from wishlist was a success.");
+        Thread.sleep(5000);
+    }
+
+@Test(description = "Adding and removing a product from shopping cart.")
+    public void shoppingCartTest() throws InterruptedException {
+checkoutPage.clickAwesomeShirtProduct();
+checkoutPage.clickShoppingCartButton();
+checkoutPage.clickShoppingIconButton();
+checkoutPage.clickTrashButton();
+    ExtentTestNGITestListener.getTest().log(Status.PASS, "Adding and removing a product from shopping cart was a success.");
+    Thread.sleep(5000);
+    }
+
+@Test(description = "Purchasing a product from wishlist.")
+    public void purchasingProductFromWishlistTest() throws InterruptedException {
+checkoutPage.clickSoftPizzaProduct();
+checkoutPage.clicWishListButton();
+checkoutPage.clickWishListIcon();
+checkoutPage.clickCartButton();
+checkoutPage.clickShoppingCartIconWish();
+checkoutPage.clickGetCheckoutButton();
+checkoutPage.setGetFirstNameSlot();
+checkoutPage.setGetLastNameSlot();
+checkoutPage.setGetAddressSlot();
+checkoutPage.clickGetContinueCheckoutButton();
+checkoutPage.clickGetCompleteOrderButton();
+checkoutPage.getOrderCompleteMessageFinal();
+    Thread.sleep(5000);
+
+
+
+
+
+
+}
+
+
+
+
 
 }
